1733df8f998ad6c50d021ac8c8a75dd52ed757c4
Anshaj <anshajshukla03@gmail.com>
2025-08-09 17:40:16 +0530

    Remove unwanted files from repository

--- load_forecast_new/src/models/src/set_backend.py
+++
@@ -1,6 +0,0 @@
-"""
-Set the matplotlib backend to a non-interactive one for headless operation.
-This should be imported before any other matplotlib imports.
-"""
-import matplotlib
-matplotlib.use('Agg')  # Use non-interactive backend
